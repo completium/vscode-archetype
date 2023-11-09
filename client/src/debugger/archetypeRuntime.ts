6a229d4a2acf2e4af119461c6eda6dcf7c2843fe
Guillaume Duhamel <guillaume.duhamel@edukera.com>
2023-11-09 16:11:29 +0100

    up

--- client/src/debugger/archetypeRuntime.ts
+++ client/src/debugger/archetypeRuntime.ts
@@ -123,35 +123,46 @@ export class ArchetypeRuntime extends EventEmitter {
 		super();
 	}
 
-	private sendEvent(event: string, ... args: any[]): void {
+	private sendEvent(event: string, ...args: any[]): void {
 		setTimeout(() => {
 			this.emit(event, ...args);
 		}, 0);
 	}
 
-	private _debugData : DebugData = null
+	private _debugData: DebugData = null
 	private _filename = ""
 	private _entrypoint = ""
-	private _inputs : EntryArg[] = []
-	private _initStorage : Storage = null
-	private _debugTrace : ArchetypeTrace = null
-	private _step : Step = null
-	private _env : ContractEnv = null
-	private _operationDetails : Map<string, Operation> = new Map<string, Operation>()
+	private _inputs: EntryArg[] = []
+	private _initStorage: Storage = null
+	private _debugTrace: ArchetypeTrace = null
+	private _step: Step = null
+	private _env: ContractEnv = null
+	private _operationDetails: Map<string, Operation> = new Map<string, Operation>()
 	private _operationChunk = 100
-	private _gasInfo : Map<number, Array<GasInfo>> = new Map<number, Array<GasInfo>>()
-	private _decorations : vscode.TextEditorDecorationType[] = []
+	private _gasInfo: Map<number, Array<GasInfo>> = new Map<number, Array<GasInfo>>()
+	private _decorations: vscode.TextEditorDecorationType[] = []
 
 	public async generateDebugData(arlFilePath: string): Promise<DebugData> {
 		try {
-   		const config = vscode.workspace.getConfiguration('archetype');
-			const archetype_exec = config.get('archetypeBin');
+			const config = vscode.workspace.getConfiguration('archetype');
 
-			// Replace with the actual path to the 'archetype' command if it's not in $PATH
-			const command = `${archetype_exec} -t debug-trace ${arlFilePath}`;
+			let output = null;
+			const archetypeMode: string = config.get('archetypeMode');
+			if (archetypeMode == 'binary' || archetypeMode == 'docker') {
+				const archetype_exec = config.get('archetypeBin');
 
-			// Execute the command
-			const output = await executeCommand(command);
+				// Replace with the actual path to the 'archetype' command if it's not in $PATH
+				const command = `${archetype_exec} -t debug-trace ${arlFilePath}`;
+
+				// Execute the command
+				output = await executeCommand(command);
+			} else {
+				const archetype = require('@completium/archetype');
+				const settings = {
+					target: 'debug-trace'
+				};
+				output = archetype.compile(arlFilePath, settings);
+			}
 
 			// Parse the command's output and cast it to the IDebug2 type
 			const debugData: DebugData = JSON.parse(output);
@@ -163,50 +174,73 @@ export class ArchetypeRuntime extends EventEmitter {
 	}
 
 	public compile(sourceFile: string): Promise<string> {
-    return new Promise((resolve, reject) => {
+		return new Promise((resolve, reject) => {
 			const config = vscode.workspace.getConfiguration('archetype');
-			const archetype_exec = config.get('archetypeBin');
+			const archetypeMode: string = config.get('archetypeMode');
 
-			// compilation is in debug mode
-      const command = `${archetype_exec} -g ${sourceFile}`;
+			// Get the system temporary directory
+			const tmpDir = os.tmpdir();
 
-      executeCommand(command)
-        .then((output) => {
-          // Get the system temporary directory
-          const tmpDir = os.tmpdir();
+			// Generate a unique file name with an extension
+			const tmpFilePath = path.join(tmpDir, `output_${Date.now()}.txt`);
 
-          // Generate a unique file name with an extension
-          const tmpFilePath = path.join(tmpDir, `output_${Date.now()}.txt`);
+			if (archetypeMode == 'binary' || archetypeMode == 'docker') {
+				const archetype_exec = config.get('archetypeBin');
 
-          // Write the command output to the temporary file
-          fs.writeFile(tmpFilePath, output, 'utf8', (err) => {
-            if (err) {
-              console.error(`Failed to write to temp file: ${err}`);
-              reject(err);
-            } else {
-              resolve(tmpFilePath);
-            }
-          });
-        })
-        .catch((error) => {
-          reject(error);
-        });
-    });
-  }
+				// compilation is in debug mode
+				const command = `${archetype_exec} -g ${sourceFile}`;
+
+				executeCommand(command)
+					.then((output) => {
+						// Write the command output to the temporary file
+						fs.writeFile(tmpFilePath, output, 'utf8', (err) => {
+							if (err) {
+								console.error(`Failed to write to temp file: ${err}`);
+								reject(err);
+							} else {
+								resolve(tmpFilePath);
+							}
+						});
+					})
+					.catch((error) => {
+						reject(error);
+					});
+			} else {
+				const settings = {
+					g: true
+				};
+				try {
+					const archetype = require('@completium/archetype');
+					const output = archetype.compile(sourceFile, settings);
+					const fs = require("fs");
+					fs.writeFile(tmpFilePath, output, 'utf8', (err) => {
+						if (err) {
+							console.error(`Failed to write to temp file: ${err}`);
+							reject(err);
+						} else {
+							resolve(tmpFilePath);
+						}
+					});
+				} catch (e) {
+					vscode.window.showErrorMessage(e);
+				}
+			}
+		});
+	}
 
 	private executeTrace(storage: string, input: string, entrypoint: string, tzSource: string): Promise<string> {
-    // Construct the command string
+		// Construct the command string
 		const config = vscode.workspace.getConfiguration('archetype');
 		const octez_client_exec = config.get('octezClientBin');
 		const base_dir = config.get('mockupBaseDir');
 
 		const parameters = `--amount '${this._env.transferred}' --source '${this._env.caller}' --now '${dateStringToSeconds(this._env.now)}' --level '${this._env.level}' --balance '${this._env.balance}'`
-    const command = `${octez_client_exec} --mode mockup --base-dir ${base_dir} run script ${tzSource} on storage '${storage}' and input '${input}' --entrypoint '${entrypoint}' ${parameters} --trace-stack`;
-    // Use the generic executeCommand function to run the command
-    return executeCommand(command)
-  }
+		const command = `${octez_client_exec} --mode mockup --base-dir ${base_dir} run script ${tzSource} on storage '${storage}' and input '${input}' --entrypoint '${entrypoint}' ${parameters} --trace-stack`;
+		// Use the generic executeCommand function to run the command
+		return executeCommand(command)
+	}
 
-	private executeDecode(v: string) : Promise<string> {
+	private executeDecode(v: string): Promise<string> {
 		const config = vscode.workspace.getConfiguration('archetype');
 		const octez_codec_exec = config.get('octezCodecBin');
 		// TODO: make protocol a parameter
@@ -214,32 +248,32 @@ export class ArchetypeRuntime extends EventEmitter {
 		return executeCommand(command)
 	}
 
-	private async getDebugTrace(program : string) : Promise<ArchetypeTrace> {
+	private async getDebugTrace(program: string): Promise<ArchetypeTrace> {
 		const tzSource = await this.compile(program)
 		const entry = this._entrypoint
 		const storage = argsToMich(this._initStorage.elements())
 		const input = argsToMich(this._inputs)
 		const trace = await this.executeTrace(storage, input, entry, tzSource)
 		const exec_trace = extract_trace(trace)
-    const contract_map_source = gen_contract_map_source(JSON.stringify(this._debugData))
-    const output = build_execution(contract_map_source, exec_trace)
+		const contract_map_source = gen_contract_map_source(JSON.stringify(this._debugData))
+		const output = build_execution(contract_map_source, exec_trace)
 		return output
 	}
 
 
-	private getEntries() : { [key: string]: number; } {
-		const entries : { [key: string]: number; } = {}
-		for (let i= 0; i< this._debugData.interface.entrypoints.length; i++) {
+	private getEntries(): { [key: string]: number; } {
+		const entries: { [key: string]: number; } = {}
+		for (let i = 0; i < this._debugData.interface.entrypoints.length; i++) {
 			entries[this._debugData.interface.entrypoints[i].name] = i
 		}
 		return entries
 	}
 
-	private getDefaultValue(arg: { name :string; type_ : string}) : string {
-		switch(arg.type_) {
-			case 'int' : case 'nat' : return '0';
+	private getDefaultValue(arg: { name: string; type_: string }): string {
+		switch (arg.type_) {
+			case 'int': case 'nat': return '0';
 			case 'timestamp': return getCurrentDateTime();
-			case 'bool' : return 'True';
+			case 'bool': return 'True';
 			default: return ""
 		}
 	}
@@ -300,12 +334,12 @@ export class ArchetypeRuntime extends EventEmitter {
 		this._entrypoint = entryname
 		const entrypoint = new EntryPoint(entryname)
 		const args = this._debugData.interface.entrypoints[entries[entryname]].args
-		for(let i=0; i < args.length; i++) {
+		for (let i = 0; i < args.length; i++) {
 			const prompt = `Argument '${args[i].name}' value:`
 			await askOpenValidate(prompt, 'Argument value', this.getDefaultValue(args[i]), (x) => entrypoint.addArg(args[i].name, x, args[i].type_))
 		}
 		const storage = new Storage()
-		for(let i=0; i<this._debugData.interface.storage.length; i++) {
+		for (let i = 0; i < this._debugData.interface.storage.length; i++) {
 			const name = this._debugData.interface.storage[i].name
 			const typ = this._debugData.interface.storage[i].type_
 			const def = this._debugData.interface.storage[i].value
@@ -324,7 +358,7 @@ export class ArchetypeRuntime extends EventEmitter {
 		this.sendEvent('stopOnEntry')
 	}
 
-	public getStorageVariables() : RuntimeVariable[] {
+	public getStorageVariables(): RuntimeVariable[] {
 		if (this.instruction == -1) {
 			return this._initStorage.elements().map(x => {
 				return new RuntimeVariable(x.name, x.value)
@@ -339,13 +373,13 @@ export class ArchetypeRuntime extends EventEmitter {
 		}
 	}
 
-	public getInputVariables() : RuntimeVariable[] {
+	public getInputVariables(): RuntimeVariable[] {
 		return this._inputs.map(x => {
 			return new RuntimeVariable(x.name, x.value)
 		})
 	}
 
-	public getConstantVariables() : RuntimeVariable[] {
+	public getConstantVariables(): RuntimeVariable[] {
 		const transferred = Number.parseInt(this._env.transferred)
 		const balance = Number.parseInt(this._env.balance)
 		const level = Number.parseInt(this._env.level)
@@ -359,26 +393,26 @@ export class ArchetypeRuntime extends EventEmitter {
 		]
 	}
 
-	private getOperationHashes() : string[] {
+	private getOperationHashes(): string[] {
 		const l = this._step.stack.filter(x => x.name == '_ops')
 		if (l.length > 0) {
 			const ops = l[0].value
 			const regex = /0x[0-9a-fA-F]+/g;
-    	return ops.match(regex);
+			return ops.match(regex);
 		}
 		return []
 	}
 
-	public async getOperations() : Promise<RuntimeVariable[]> {
+	public async getOperations(): Promise<RuntimeVariable[]> {
 		const hashes = this.getOperationHashes()
 		let ops = hashes.map((h, i) => {
 			return new RuntimeVariable(i + "", h)
 		})
-		for (let i=0; i < hashes.length; i++) {
+		for (let i = 0; i < hashes.length; i++) {
 			const h = hashes[i]
 			try {
 				const res = await this.executeDecode(h)
-				const op : Operation = parseToOperation(res)
+				const op: Operation = parseToOperation(res)
 				this._operationDetails.set(h, op)
 				// set reference
 				ops[i].reference = this._operationChunk + i
@@ -389,7 +423,7 @@ export class ArchetypeRuntime extends EventEmitter {
 		return ops
 	}
 
-	public getOperationDetail(variableReference : number) : RuntimeVariable[] {
+	public getOperationDetail(variableReference: number): RuntimeVariable[] {
 		const hashes = this.getOperationHashes()
 		const opIdx = variableReference % this._operationChunk
 		const opHash = hashes[opIdx]
@@ -397,8 +431,8 @@ export class ArchetypeRuntime extends EventEmitter {
 		const displayLevel = Math.floor(variableReference / this._operationChunk);
 		if (displayLevel == 1) {
 			let base = [new RuntimeVariable("kind", opDetail.kind)]
-			switch(opDetail.kind) {
-				case 'transaction' : {
+			switch (opDetail.kind) {
+				case 'transaction': {
 					base = base.concat([
 						new RuntimeVariable("amount", Number.parseInt(opDetail.amount)),
 						new RuntimeVariable("destination", opDetail.destination)
@@ -406,21 +440,21 @@ export class ArchetypeRuntime extends EventEmitter {
 					if (opDetail.parameters != undefined) {
 						let params = new RuntimeVariable("parameters", "")
 						params.reference = 2 * this._operationChunk + opIdx
-						base = base.concat([ params ])
+						base = base.concat([params])
 					}
 				}; break;
-				case 'delegation' : {
+				case 'delegation': {
 					base = base.concat([
 						new RuntimeVariable("delegate", opDetail.delegate)
 					])
 				}; break;
-				case 'event' : {
+				case 'event': {
 					base = base.concat([
 						new RuntimeVariable("type", opDetail.type),
 						new RuntimeVariable("payload", opDetail.payload)
 					])
 				}; break;
-				case 'origination' : {
+				case 'origination': {
 					let script = new RuntimeVariable("script", JSON.stringify(opDetail.script))
 					script.reference = 2 * this._operationChunk + opIdx
 					base = base.concat([
@@ -431,7 +465,7 @@ export class ArchetypeRuntime extends EventEmitter {
 			}
 			return base
 		} else if (displayLevel == 2) {
-			switch(opDetail.kind) {
+			switch (opDetail.kind) {
 				case 'transaction':
 					return [
 						new RuntimeVariable("entrypoint", opDetail.parameters.entrypoint),
@@ -458,8 +492,8 @@ export class ArchetypeRuntime extends EventEmitter {
 		}
 	}
 
-	public getLocalVariables() : RuntimeVariable[] {
-		if(this.instruction >= 0) {
+	public getLocalVariables(): RuntimeVariable[] {
+		if (this.instruction >= 0) {
 			return this._step.stack.filter(x => {
 				return x.name != '_ops' && !this._initStorage.elements().some(item => item.name == x.name) && !this._inputs.some(item => item.name == x.name)
 			}).map(x => {
@@ -467,29 +501,29 @@ export class ArchetypeRuntime extends EventEmitter {
 			})
 		}
 		return []
- 	}
+	}
 
-	public evaluate(name: string) : RuntimeVariable | undefined {
+	public evaluate(name: string): RuntimeVariable | undefined {
 		const storageVars = this.getStorageVariables()
-		for(let i=0; i<storageVars.length; i++) {
+		for (let i = 0; i < storageVars.length; i++) {
 			if (storageVars[i].name == name) {
 				return storageVars[i]
 			}
 		}
 		const localVars = this.getLocalVariables()
-		for(let i=0; i<localVars.length; i++) {
+		for (let i = 0; i < localVars.length; i++) {
 			if (localVars[i].name == name) {
 				return localVars[i]
 			}
 		}
 		const inputVars = this.getInputVariables()
-		for(let i=0; i<inputVars.length; i++) {
+		for (let i = 0; i < inputVars.length; i++) {
 			if (inputVars[i].name == name) {
 				return inputVars[i]
 			}
 		}
 		const constants = this.getConstantVariables()
-		for(let i=0; i<constants.length; i++) {
+		for (let i = 0; i < constants.length; i++) {
 			if (constants[i].name == name) {
 				return constants[i]
 			}
@@ -500,7 +534,7 @@ export class ArchetypeRuntime extends EventEmitter {
 	// This is the next instruction that will be 'executed'
 	public instruction = -1;
 
-	private displayStep(step : Step) {
+	private displayStep(step: Step) {
 		console.log(step)
 	}
 
@@ -523,7 +557,7 @@ export class ArchetypeRuntime extends EventEmitter {
 
 	}
 
-	public stack() : IRuntimeStack  {
+	public stack(): IRuntimeStack {
 		const frames: IRuntimeStackFrame[] = [];
 		let step = this._debugTrace.steps[this.instruction + 1];
 		let line = 0
@@ -544,13 +578,13 @@ export class ArchetypeRuntime extends EventEmitter {
 			endColumn = step.range.end.col
 		}
 		frames.push({
-			index      : this.instruction,
-			name       : this._entrypoint,
-			file       : this._filename,
-			line       : line,
-			column     : column,
-			endLine    : endLine,
-			endColumn  : endColumn,
+			index: this.instruction,
+			name: this._entrypoint,
+			file: this._filename,
+			line: line,
+			column: column,
+			endLine: endLine,
+			endColumn: endColumn,
 			instruction: this.instruction
 		})
 
