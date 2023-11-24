5a4f7e8f44bba955eb6f33a558463958bea9b670
Guillaume Duhamel <guillaume.duhamel@edukera.com>
2023-11-24 01:06:50 +0100

    up

--- client/src/activateArchetypeDebug.ts
+++ client/src/activateArchetypeDebug.ts
@@ -6,7 +6,7 @@ import { CancellationToken, DebugConfiguration, ProviderResult, WorkspaceFolder 
 import { ArchetypeDebugSession } from './archetypeDebug';
 import { FileAccessor } from './archetypeRuntime';
 
-export function activateArchetypeDebug(context: vscode.ExtensionContext, factory?: vscode.DebugAdapterDescriptorFactory) {
+export function activateArchetypeDebug(context: vscode.ExtensionContext) {
 
 	context.subscriptions.push(
 		vscode.commands.registerCommand('extension.archetype-debug.runEditorContents', (resource: vscode.Uri) => {
@@ -86,71 +86,14 @@ export function activateArchetypeDebug(context: vscode.ExtensionContext, factory
 		}
 	}, vscode.DebugConfigurationProviderTriggerKind.Dynamic));
 
-	if (!factory) {
-		factory = new InlineDebugAdapterFactory();
-	}
+	const factory = new InlineDebugAdapterFactory();
 	context.subscriptions.push(vscode.debug.registerDebugAdapterDescriptorFactory('archetype', factory));
-	// if ('dispose' in factory) {
-	// 	context.subscriptions.push(factory);
-	// }
-
-	// override VS Code's default implementation of the debug hover
-	// here we match only Mock "variables", that are words starting with an '$'
-	context.subscriptions.push(vscode.languages.registerEvaluatableExpressionProvider('archetype', {
-		provideEvaluatableExpression(document: vscode.TextDocument, position: vscode.Position): vscode.ProviderResult<vscode.EvaluatableExpression> {
-
-			const VARIABLE_REGEXP = /\$[a-z][a-z0-9]*/ig;
-			const line = document.lineAt(position.line).text;
-
-			let m: RegExpExecArray | null;
-			while (m = VARIABLE_REGEXP.exec(line)) {
-				const varRange = new vscode.Range(position.line, m.index, position.line, m.index + m[0].length);
-
-				if (varRange.contains(position)) {
-					return new vscode.EvaluatableExpression(varRange);
-				}
-			}
-			return undefined;
-		}
-	}));
-
-	// override VS Code's default implementation of the "inline values" feature"
-	context.subscriptions.push(vscode.languages.registerInlineValuesProvider('markdown', {
-
-		provideInlineValues(document: vscode.TextDocument, viewport: vscode.Range, context: vscode.InlineValueContext) : vscode.ProviderResult<vscode.InlineValue[]> {
-
-			const allValues: vscode.InlineValue[] = [];
-
-			for (let l = viewport.start.line; l <= context.stoppedLocation.end.line; l++) {
-				const line = document.lineAt(l);
-				var regExp = /\$([a-z][a-z0-9]*)/ig;	// variables are words starting with '$'
-				do {
-					var m = regExp.exec(line.text);
-					if (m) {
-						const varName = m[1];
-						const varRange = new vscode.Range(l, m.index, l, m.index + varName.length);
-
-						// some literal text
-						//allValues.push(new vscode.InlineValueText(varRange, `${varName}: ${viewport.start.line}`));
-
-						// value found via variable lookup
-						allValues.push(new vscode.InlineValueVariableLookup(varRange, varName, false));
-
-						// value determined via expression evaluation
-						//allValues.push(new vscode.InlineValueEvaluatableExpression(varRange, varName));
-					}
-				} while (m);
-			}
-
-			return allValues;
-		}
-	}));
 }
 
 class ArchetypeConfigurationProvider implements vscode.DebugConfigurationProvider {
 
 	/**
-	 * Massage a debug configuration just before a debug session is being launched,
+	 * Message a debug configuration just before a debug session is being launched,
 	 * e.g. add all missing attributes to the debug configuration.
 	 */
 	resolveDebugConfiguration(folder: WorkspaceFolder | undefined, config: DebugConfiguration, token?: CancellationToken): ProviderResult<DebugConfiguration> {
@@ -158,7 +101,7 @@ class ArchetypeConfigurationProvider implements vscode.DebugConfigurationProvide
 		// if launch.json is missing or empty
 		if (!config.type && !config.request && !config.name) {
 			const editor = vscode.window.activeTextEditor;
-			if (editor && editor.document.languageId === 'markdown') {
+			if (editor && editor.document.languageId === 'archetype') {
 				config.type = 'archetype';
 				config.name = 'Launch';
 				config.request = 'launch';
